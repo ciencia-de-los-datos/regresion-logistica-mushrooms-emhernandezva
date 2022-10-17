708bc2bdeca82046c5fc38ae248271c48c37f75b
emhernandezva <emhernandezva@unal.edu.co>
2022-10-17 16:58:14 -0500

    Preguntas 1 y 2

--- preguntas.py
+++ preguntas.py
@@ -64,7 +64,7 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('/content/drive/MyDrive/Especialización Analítica/Ciencia de los Datos Aplicada/Talleres/Taller 9/mushrooms.csv')
+    df = pd.read_csv('mushrooms.csv')
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
